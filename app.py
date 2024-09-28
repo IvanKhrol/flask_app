0573d60c480e1a7e6b94ebe05194e90ff906eb72
Хрол Иван Андреевич <i.khrol@okbsapr.ru>
2024-09-28 10:02:25 +0300

    Added primitive output of DB data

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+import sqlite3
 
 from flask import Flask
+
+DB_PATH = '/home/ent1r/study/bd/DB/test.s3db'
 
 app = Flask(__name__) # Создаем экземпляр класса Flask и указываем ему имя модуля откуда брать настройки
 
 @app.route('/') # Декоратор - обработчик определенных запросов/событий
 def main_page():
-  return  'Hello world'
+  with sqlite3.connect(DB_PATH) as conn:
+    cur = conn.cursor()
+    cur.execute('Select * FROM t1')
+  return  str(cur.fetchall())
 
 if __name__ == '__main__':
   app.run(port=5010, debug=True) # Запускаем на нужном порте с отладочной инфой
